623656b3afd557fbbdafedc9a12ef6aa2821aa3c
ollinieminen <otn987@gmail.com>
2019-06-03 09:22:13 -0700

    initialize gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,58 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import {StaticQuery, graphql } from "gatsby";
+import Layout from "../components/layout";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+
+import Profile from '../components/Profile';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import {  faCoffee, faPhone } from '@fortawesome/free-solid-svg-icons';
+
+
+
+ 
+library.add(fab, faCoffee, faPhone );
+
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+
+
+  <StaticQuery query={graphql`
+  {
+    faceShot: file(relativePath: {eq:"OllieNieminen.png"}){
+          childImageSharp{
+            fluid(maxWidth:1600){
+              ...GatsbyImageSharpFluid
+            }
+          }
+    }
+banner: file(relativePath: {eq:"banner.jpg"}){
+  childImageSharp{
+    fluid(maxWidth:1400){
+      ...GatsbyImageSharpFluid
+    }
+  }
+}
+smallProfile: file(relativePath: {eq:"ThumbnailHeadShot.jpg"}){
+  childImageSharp{
+    fluid(maxWidth:1400){
+      ...GatsbyImageSharpFluid
+    }
+  }
+}
+
+  }
+    `
+  } render={(data) => {
+      return (
+        <Layout>
+          <Profile data={data}/>
+        </Layout>
+      )
+    }} />
+
+
 )
 
 export default IndexPage
