fe851182f3f5d5470cef5d7682a0f95880984c8a
ollinieminen <otn987@gmail.com>
2019-06-08 15:25:28 -0700

    added pdf transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,14 @@ module.exports = {
     
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    
+    },
+    {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
